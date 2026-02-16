748c8957beefc3ec4cc6eeec51dda725ff9bd66c
alejanf2885 <alumno.648000@ies-azarquiel.es>
2026-02-16 18:43:17 +0100

    Add project files.

---
+++ VeterinariaMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VeterinariaMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

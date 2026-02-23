066e6afc10bdd1103a563cf51e3606b29cc857c1
alejanf2885 <alumno.648000@ies-azarquiel.es>
2026-02-23 20:46:49 +0100

    Add Usuario model and initial project documentation
    Added Usuario.cs with SQL Server mapping for user management.

---
+++ VeterinariaMvc/Models/Usuario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VeterinariaMvc.Models
+{
+    [Table("USUARIO")]
+    public class Usuario
+    {
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Column("EMAIL")]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        [Column("PASSWORD")]
+        public string Password { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Column("NOMBRE")]
+        public string Nombre { get; set; }
+
+        [MaxLength(20)]
+        [Column("TELEFONO")]
+        public string Telefono { get; set; }
+
+        [Column("ACTIVO")]
+        public bool Activo { get; set; } = true;
+
+        [Column("FECHA_CREACION")]
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+
+        [Column("ID_ROL")]
+        public int IdRol { get; set; }
+
+       
+    }
+}
